28933a24f218fb1bf1eaa128ec398b7e85fa8aab
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-03-13 12:33:02 +0000

    Done until 12.1

--- .learn/resets/12-Map_a_list/app.py
+++ .learn/resets/12-Map_a_list/app.py
@@ -2,8 +2,7 @@ celsius_values = [-2, 34, 56, -10]
 
 def celsius_to_fahrenheit(celsius):
     # The magic happens here
-   fht = celsius * 9/5 + 32
-   return fht
+   
 
 result = list(map(celsius_to_fahrenheit, celsius_values))
 
